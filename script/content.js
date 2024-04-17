b5aa753fc2d4123e17d91aa390603631b461c7a6
William Di Biasi Bogik <williamcms_@outlook.com>
2024-04-17 11:57:49 -0300

    Added an API call for the menu

--- script/content.js
+++ script/content.js
@@ -30,4 +30,29 @@ function addContent() {
 
     return response
   }
+
+  ;(loadMenu = () => {
+    postData(
+      {
+        CodigoCliente: store.clientId,
+        IDCatalogo: catalog.id,
+      },
+      '/Produtos/Linhas'
+    ).then((data) => {
+      if (data?.length === 0) return
+
+      let html = ''
+      let url = `/Produtos/Catalogo?CodCliFor=${store.clientId}&IDCatalogo=${catalog.id}`
+
+      data?.forEach(({ codigo, descricao }) => {
+        let urlModified = `${url}&Linha=${codigo}`
+
+        html += `<li class="menu--item"><a href="${urlModified}" target="_self" class="menu--itemLink">${descricao}</a></li>`
+      })
+
+      html += `<li class="menu--item menu--allItems"><a href="${url}" target="_self" class="menu--itemLink">Ver todas as categorias</a></li>`
+
+      schema.menu?.html(html)
+    })
+  })()
 }
